9cc1140eeffaf45ff08816c8daed4e1c65c764d4
张维 <zhangwei>
2019-04-10 17:25:03 +0800

    1.异常时信息保存到文件中

--- BaiDuTranslate.py
+++ BaiDuTranslate.py
@@ -15,19 +15,22 @@ def translate(query, fromLanguage='auto', toLanguage='zh'):
     m2.update(src.encode('utf-8'))
     try:
         responseBaidu = requests.get(urlBaiduTranslate,
-                                 {
-                                     'q': query,
-                                     'from': fromLanguage,
-                                     'to': toLanguage,
-                                     'appid': appid,
-                                     'salt': salt,
-                                     'sign': m2.hexdigest()
-                                 })
+                                     {
+                                         'q': query,
+                                         'from': fromLanguage,
+                                         'to': toLanguage,
+                                         'appid': appid,
+                                         'salt': salt,
+                                         'sign': m2.hexdigest()
+                                     })
         json = responseBaidu.json()
         print(json)
     except Exception as err:
         print('Error Msg')
         print(err)
+        s_open = open('log.txt', 'a+')
+        s_open.write("\n\n" + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + "\n" + str(err) + "\n")
+        s_open.close()
         return '错误:' + str(err)
     return json
 
@@ -35,6 +38,8 @@ def translate(query, fromLanguage='auto', toLanguage='zh'):
 def getTranslateValue(query, fromLanguage='auto', toLanguage='zh'):
     res = translate(query, fromLanguage, toLanguage)
     if 'error_code' in res:
+        return str(res)
+    elif '错误:' in res:
         return str(res)
     else:
         return res['trans_result'][0]['dst']
